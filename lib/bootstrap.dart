6ccb2ae950e676b65f6a8d628be53f5d0e1a0730
Corvus400 <13657682+Corvus400@users.noreply.github.com>
2026-05-03 21:37:28 +0900

    feat: wire global error handlers (FlutterError, PlatformDispatcher, runZonedGuarded)

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,7 +1,33 @@
-import 'package:flutter/widgets.dart';
+import 'dart:async';
 
-/// Starts the Flutter application.
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:logger/logger.dart';
+
+/// Shared application logger.
+final logger = Logger();
+
+/// Starts the Flutter application with global error handlers.
 void bootstrap(Widget Function() builder) {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(builder());
+  FlutterError.onError = (details) {
+    logger.e(
+      'FlutterError',
+      error: details.exception,
+      stackTrace: details.stack,
+    );
+    FlutterError.presentError(details);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    logger.e('PlatformDispatcher', error: error, stackTrace: stack);
+    return true;
+  };
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(builder());
+    },
+    (error, stack) {
+      logger.e('Zone', error: error, stackTrace: stack);
+    },
+  );
 }
