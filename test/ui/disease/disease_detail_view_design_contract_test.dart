f5adddd5002444b539104b465e2bde6d3b1ffa9a
Corvus400 <13657682+Corvus400@users.noreply.github.com>
2026-05-08 00:17:09 +0900

    test(ui/detail): add split detail verification entrypoints

---
+++ test/ui/disease/disease_detail_view_design_contract_test.dart
@@ -0,0 +1,5 @@
+import 'disease_detail_view_test.dart' as disease_detail_view_test;
+
+void main() {
+  disease_detail_view_test.main();
+}
---
+++ test/ui/drug/drug_detail_view_design_contract_test.dart
@@ -0,0 +1,5 @@
+import 'drug_detail_view_test.dart' as drug_detail_view_test;
+
+void main() {
+  drug_detail_view_test.main();
+}
