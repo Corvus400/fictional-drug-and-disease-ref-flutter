95e574dc02f674ae99c4dce1a7107ebf02b75bfb
Corvus400 <13657682+Corvus400@users.noreply.github.com>
2026-05-13 20:43:24 +0900

    test(ci): update early VRT diff

--- lib/ui/disease/widgets/disease_detail_overview_tab.dart
+++ lib/ui/disease/widgets/disease_detail_overview_tab.dart
@@ -69,7 +69,7 @@ class _DiseaseHero extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(
           horizontal: DetailConstants.heroMetaPaddingHorizontal,
-          vertical: DetailConstants.heroMetaPaddingVertical + 4,
+          vertical: DetailConstants.heroMetaPaddingVertical + 8,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
